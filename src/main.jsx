9562f00ec43e362e8370914c50f4d97e8584baf9
EinarGatc <einar.gatchalian@gmail.com>
2025-03-17 11:12:22 -0700

    base code

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import './index.css'
 // import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    just checking
+    just checking fdhaljfdakfhdlanfvkldnalfdklad
     {/* <BrowserRouter basename='/portfolio/'>
       <Routes>
         <Route path='*' element={<App />}/>
