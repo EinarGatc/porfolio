f15acf2fe72e7c90ec74be161e93dab48ceb9321
EinarGatc <einar.gatchalian@gmail.com>
2025-03-17 10:54:18 -0700

    r1

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,11 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/portfolio/'>
+    just checking
+    {/* <BrowserRouter basename='/portfolio/'>
       <Routes>
         <Route path='*' element={<App />}/>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
   </StrictMode>
 )
